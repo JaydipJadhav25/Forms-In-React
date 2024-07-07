d687ca592499834c36534e0436ea1b0627d6b3c2
jaydipjadhav <jaydipdjadhav>
2024-07-07 16:24:53 +0530

    use useRef hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useRef } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const emailRef = useRef(null);
+  const btnRef = useRef(null);
+
+const  handlfocus = () =>{
+  console.log(emailRef);
+  console.log(btnRef);
+  emailRef.current.focus();
+  emailRef.current.style.backgroundColor = "red";
+  emailRef.current.placeholder = "done";
+
+  console.log(emailRef.current.value)
+
+  
+}
+ 
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='mt-5'>
+        <input type='email' placeholder='enter email'  ref={emailRef}/>
+        <button onClick={handlfocus} ref={btnRef} >set Focus</button>
+
+
+        
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
